48dad27c382a3bf318f511f58cb6165c6fc7060b
joris <weets.joris@gmail.com>
2020-02-24 15:47:20 +0100

    avancé carte emplacement tour

--- src/epictgc/Carte.java
+++ src/epictgc/Carte.java
@@ -1,5 +1,24 @@
 package epictgc;
 
 public class Carte {
+	
+	public Carte() {
+		
+	}
+	public boolean poser(Emplacement emplacement , Tour tour)
+	{
+		if (emplacement.estVide) 
+		{
+			// on pose la carte
+			//TODO terrain
+		}
+		
+		return true;
+	}
+	
+	public boolean defausse() 
+	{
+		return false;
+	}
 
 }
--- src/epictgc/Emplacement.java
+++ src/epictgc/Emplacement.java
@@ -1,5 +1,19 @@
 package epictgc;
 
 public class Emplacement {
-
+	boolean estVide;
+	public Emplacement() 
+	{
+		estVide = true;	
+	}
+	
+	public boolean estvide()
+	{
+		return estVide;
+	}
+	public void setestVide(boolean b)
+	{
+		estVide = b;
+	}
 }
+ 
