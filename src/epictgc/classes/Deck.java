d60fb82a475db41e821c538cd514a3e6258c1f8b
Yjla <decaux59178@gmail.com>
2020-02-26 23:00:28 +0100

    Ajout méthode add et addall pour ajouter une ou plusieurs cartes dans le deck

--- src/epictgc/classes/Deck.java
+++ src/epictgc/classes/Deck.java
@@ -24,12 +24,49 @@ public class Deck implements IDeck
 	}
 	
 	/*
-	 * Méthode de pioche, on diminue le nombre de carte du deck
-	 * et on retire et retourne la première carte du deck
+	 * Methode de pioche, on diminue le nombre de carte du deck
+	 * et on retire et retourne la premiere carte du deck
 	 */
 	public Carte draw() {
 		nbCard--;
 		return this.cards.remove(0);
+	}
+	
+	/*
+	 * methode d'ajout d'une carte au deck
+	 * retourne faux si ne peut pas rajouter
+	 */
+	public boolean add(Carte card) {
+		if (this.cards.size() < nbMaxCard) {
+			this.cards.add(card);
+			return true;
+		}
+		return false;
+	}
+	
+	/*
+	 * methode d'ajout de plusieurs cartes dans le deck
+	 * retourne vrai tant qu'au moins une carte a ete ajoutee
+	 * si aucune carte n'est ajoutee au deck, retourne faux
+	 */
+	public boolean addAll(List<Carte> cards) {
+		if (this.cards.size() + cards.size() <= nbMaxCard) {
+			cards.addAll(cards);
+			nbCard += cards.size();
+			return true;
+		}
+		else if (this.cards.size() >= this.nbMaxCard){
+			return false;
+		}
+		else {
+			for (Carte card : cards) {
+				if(this.cards.size() >= this.nbMaxCard) {
+					return true;
+				}
+				this.cards.add(card);
+			}
+		}
+		return true;
 	}
 	
 	
--- src/epictgc/interfaces/IDeck.java
+++ src/epictgc/interfaces/IDeck.java
@@ -1,7 +1,12 @@
 package epictgc.interfaces;
 
+import java.util.List;
+
 import epictgc.classes.Carte;
 
 public interface IDeck {
 	public Carte draw();
+	public boolean add(Carte card);
+	public boolean addAll(List<Carte> cards);
+	
 }
